afd17ec3da454b334f8c822fdc2c2042b1be5557
Nousheen-Adeel <nousheenfatimaadeel@gmail.com>
2025-02-04 11:41:50 +0500

    update

--- src/components/Footer1.tsx
+++ src/components/Footer1.tsx
@@ -4,7 +4,7 @@ const Footer1 = () => {
   return (
     <div className="flex flex-col md:flex-row justify-evenly text-center bg-black text-white py-10 px-6">
         <p className="text-sm sm:text-base lg:text-lg mb-4">
-          © 2025 The Psychology of Social Media | Made by Nousheen Fatima Adeel
+          © 2025 Ecommerce-Flower-Website | Made by Nousheen Fatima Adeel
         </p>
     </div>
   )
